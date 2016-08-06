eacd47699c28bea48d7a04cf00de4c10bf356dfa
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-08-06 10:56:10 -0500

    cleanup forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ def create
 	user = User.find_by(email: params[:session][:email].downcase)
 	if user && user.authenticate(params[:session][:password])
 		log_in user
+		flash[:notice] = "Successfully logged in"
 		redirect_to user
 	else
 		flash[:notice] = "Invalid email/password combination"
