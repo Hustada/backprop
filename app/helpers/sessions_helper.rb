4988d60856c8707841f2d35658080c1da52fca60
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-08-16 00:04:57 -0500

    add forget method to user model

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,7 +16,7 @@ module SessionsHelper
 		@current_user ||= User.find_by(id: user_id)
 	elsif (user_id = cookies.signed[:user_id])
 		user = User.find_by(id: user_id)
-		if user && user.authenticated?(cookies[:remember_token])
+		if user && user.authenticated?(:remember, cookies[:remember_token])
 			log_in user
 			@current_user = user
 		end
@@ -29,12 +29,14 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+#forgets a persistent session.
 	def forget(user)
 		user.forget
 		cookies.delete(:user_id)
 		cookies.delete(:remember_token)
 	end
 
+#logs out the current user
 	def log_out
 		forget(current_user)
 		session.delete(:user_id)
