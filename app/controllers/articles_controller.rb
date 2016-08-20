d79b003a8ead76a34ceb0a4297d780262815c634
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-08-20 09:35:50 -0500

    added a bunch of shit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,13 +18,17 @@ class ArticlesController < ApplicationController
 	end
 
 	def index
-		@articles = Article.all.order('created_at DESC').paginate(page: params[:page],per_page: 4)
+		@articles = Article.all.order('created_at DESC').paginate(page: params[:page],per_page: 2)
 	end
 
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to root_path
+	end
+
+	def truncation
+		@articles = Article.all.truncate(40)
 	end
 
 
