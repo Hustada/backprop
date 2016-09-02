c198db9d505f7b2f713cbc6d261b17b9d1c9268d
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-09-02 10:31:34 -0500

    include assets in assests.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,9 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( jquery.slidereveal.js, custom.js, custom.css.scss, skeleton.css )
+Rails.application.config.assets.precompile += %w( 'slidereveal.js',
+ 'custom.js',
+ 'custom.css.scss',
+ 'skeleton.css.scss',
+ 'ionicons.min.css',
+ 'animate.css' )
