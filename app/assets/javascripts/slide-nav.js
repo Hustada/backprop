f3c863fa904222c3a1aa535744fabce36bd185fd
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-09-05 12:08:44 -0500

    some small changes

---
+++ app/assets/javascripts/slide-nav.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+ // $("#js--hamburger").click(function(){
+ //    $(this).addClass(".nav-show")
+ //  });
+
+ $("#js--hamburger").click(function(){
+$(".nav-wrap").slideToggle();
+});
+});
