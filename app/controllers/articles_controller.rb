08101ed82e329641d6ec2056a8efd80b740ea566
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-11-05 12:48:56 -0500

    add edit to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,15 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(params[:article].permit(:title, :body, :image, :header))
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
