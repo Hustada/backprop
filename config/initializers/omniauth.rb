bac7520d942d32e7d0e65a1d15b41d85f6dd1aef
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-11-27 08:59:41 -0600

    remove omniauth file, rake assets, change iframe border

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, 117477670965-kjlkcu3516utvtv3m0vsq6a9fufj24jh.apps.googleusercontent.com, TDkKof3RzIuXm_il8QR7wBR3, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
