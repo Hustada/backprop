b80fbb1e7401de989ad61145eeefb8cda86814da
Mark Hustad <hustada80@gmail.com>
2017-01-16 18:27:22 -0600

    remove button and some model changes, weight belongs to workout

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,5 +1,6 @@
 class Weight < ApplicationRecord
 	belongs_to :exercise
+	belongs_to :workout
 	validates :amount, presence: true, length: { maximum: 255 }
 	validates :reps, presence: true, length: { maximum: 255 }
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,6 +2,7 @@ class Workout < ApplicationRecord
 	belongs_to :user
 	has_many :exercises
 	has_many :weights, through: :exercises
+	accepts_nested_attributes_for :exercises, :allow_destroy => true
 	accepts_nested_attributes_for :weights, :allow_destroy => true
 	validates :bodypart, presence: true, length: { maximum: 255 }
 end
