8d7fbbae5b7f0a24fdce4c05ce6f71dddbcf2d89
Mark Hustad <hustada80@gmail.com>
2017-03-22 20:17:02 -0500

    tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,15 +56,14 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "experimental_#{Rails.env}"
 
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { host: 'http://still-garden-66983.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
+    domain: 'http://still-garden-66983.herokuapp.com',
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
