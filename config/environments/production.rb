29a5df9116cd47514080b3d109a4fd8ea5830aae
Mark Hustad <hustada80@gmail.com>
2017-03-23 00:11:51 -0500

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,10 +59,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'http://still-garden-66983.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'http://still-garden-66983.herokuapp.com',
+    domain: 'gmail.com',
     authentication: 'plain', 
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'],
