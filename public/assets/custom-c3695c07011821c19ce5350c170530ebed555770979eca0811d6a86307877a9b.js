f50f716e7cfc846be123334d4f0a2c8c63c7b65b
Mark Hustad <hustada80@gmail.com>
2017-06-22 15:58:19 -0500

    smooth down scrolling

---
+++ public/assets/custom-c3695c07011821c19ce5350c170530ebed555770979eca0811d6a86307877a9b.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.flash-message').delay(3000).slideDown('slow').delay(500).slideUp('slow');
+
+    function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+    $(window).scroll(collapseNavbar);
+    $(document).ready(collapseNavbar);
+
+    $(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500 );
+        event.preventDefault();
+    });
+});
+});
