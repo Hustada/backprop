002630b4af2ca79037e428ec407e373f0dbd3855
Mark Hustad <hustada80@gmail.com>
2017-10-20 15:09:30 -0500

    changes to sendgrid action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,13 +69,13 @@ Rails.application.configure do
 #     password: ENV['GMAIL_PASSWORD'],
 #     enable_starttls_auto: true,
 # }
-
+    config.action_mailer.default_url_options = { :host => "still-garden-66983.herokuapp.com" }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'still-garden-66983.herokuapp.com',
+      :domain => 'backpropagate.com',
       :address => 'smtp.sendgrid.net',
       :port => 587,
       :authentication => :plain,
