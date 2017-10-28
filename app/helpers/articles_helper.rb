2b6fab875529d205bb4eec9c8c055acefc546f38
Mark Hustad <hustada80@gmail.com>
2017-10-28 15:03:08 -0500

    randomize images for articles articles that have no uploaded images

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -8,5 +8,12 @@ module ArticlesHelper
 	def video_id(video_url)
 		youtube_id = video_url.split("=").last
 	end
+
+#Gets a random image in case no image has been uploaded
+	def get_random_image
+    image_path_prefix = "app/assets/images/"
+    image_files = Dir.glob("#{image_path_prefix}random-images/*")
+    image_files.sample.split(image_path_prefix)[1]
+  end
 	
 end
