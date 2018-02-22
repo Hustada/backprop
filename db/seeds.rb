5b210a5d90452036da3929feadae5710e6712415
Mark Hustad <mark.hustad@firespring.com>
2018-02-22 13:06:53 -0600

    some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,13 @@
 
 #Article.create(title: 'Something Here', body: 'Lorem ipsum dolor sit amet consectetur adipiscing elit hac felis metus litora habitasse egestas, integer habitant tellus leo aptent nulla dictumst molestie cum aliquam eget. Felis urna libero dictum eu purus scelerisque accumsan laoreet fringilla massa porta vel, nisi cras nulla ut iaculis consequat vivamus ac mauris tellus diam ultrices ultricies, gravida quam sodales leo egestas curae nascetur in integer tortor rutrum. Libero mus risus ridiculus et in integer primis, eleifend torquent praesent ad tempus justo nullam, egestas eu placerat at etiam imperdiet. Nec risus nibh malesuada aliquet curabitur pharetra porttitor, augue convallis cubilia dignissim facilisi tincidunt sollicitudin etiam, egestas fusce mi nisi sapien pretium. Potenti morbi gravida pharetra risus ridiculus vivamus nostra felis porttitor, est vulputate urna ornare turpis elementum ac nullam, tortor sem torquent inceptos accumsan bibendum sed litora.')
 User.create(email: 'hustad80@gmail.com', password: 'frickers', password_confirmation: 'frickers', admin: true, username: 'hustada', confirmed_at: '2018-02-12 16:25:26.201266')
-# (1..100).each do |i|
-#   Article.create!(title: "Lipsum Post #{i}", body: %{
-#       Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat purus dapibus fermentum sagittis. Fusce in tempus felis. Phasellus a erat ut lorem lacinia bibendum. Vivamus viverra facilisis neque, in scelerisque urna pharetra vel. Donec a est mauris. Integer eget metus quis eros egestas elementum. Integer bibendum risus hendrerit dapibus tempor. Fusce placerat in orci vitae tincidunt.
-#   },
-#   image: File.new(Rails.root + "app/assets/images/serversinside.jpg"),
-#   header: 	File.new(Rails.root + "app/assets/images/darkcode.jpg"))
-# end
+
+
+(1..10).each do |i|
+  Article.create!(title: "Lipsum Post #{i}", body: %{
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat purus dapibus fermentum sagittis. Fusce in tempus felis. Phasellus a erat ut lorem lacinia bibendum. Vivamus viverra facilisis neque, in scelerisque urna pharetra vel. Donec a est mauris. Integer eget metus quis eros egestas elementum. Integer bibendum risus hendrerit dapibus tempor. Fusce placerat in orci vitae tincidunt.
+  },
+  image: File.new(Rails.root + "app/assets/images/darkcode.jpg"),
+  header: 	File.new(Rails.root + "app/assets/images/darkcode.jpg"),
+  video_url: "https://www.youtube.com/watch?v=uvuu6SJZkOQ")
+end
