d605b13073564af7806836f0101e19f281075073
Mark Hustad <hustada80@gmail.com>
2018-02-28 09:51:54 -0600

    precompile assets

---
+++ public/assets/pagination-1545172543122b42021bd7a3841b36b8ca0d80f6d09ccb78416385170d43c694.js
@@ -0,0 +1,16 @@
+document.addEventListener("turbolinks:load", function() {
+    jQuery(function() {
+    if ($('#infinite-scrolling').size() > 0) {
+      return $(window).on('scroll', function() {
+        var more_posts_url; // <- /events?page=2
+        more_posts_url = $('.pagination .next_page a').attr('href');
+        if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 250) {
+          $('.pagination').html('<img src="../assets/images/cool-loading.gif" alt="Loading..." title="Loading..." />');
+          $.getScript(more_posts_url);
+        }
+        return;
+      });
+    }
+  });
+})
+;
