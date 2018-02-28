8e2e36d3a5aa79064863e4798adbe81ce2c54224
Mark Hustad <hustada80@gmail.com>
2018-02-28 09:51:10 -0600

    some changes

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -5,7 +5,7 @@ document.addEventListener("turbolinks:load", function() {
         var more_posts_url; // <- /events?page=2
         more_posts_url = $('.pagination .next_page a').attr('href');
         if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 250) {
-          $('.pagination').html('<img src="../assets/ajax-loader.gif" alt="Loading..." title="Loading..." />');
+          $('.pagination').html('<img src="../assets/images/cool-loading.gif" alt="Loading..." title="Loading..." />');
           $.getScript(more_posts_url);
         }
         return;
